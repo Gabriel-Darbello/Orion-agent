cccd54b7b5f15a5178fdd21669a9eb9d99256992
Gabriel Darbello de Oliveira <gadarbello@gmail.com>
2026-05-30 19:00:45 -0300

    feat: CLI interface

---
+++ interface/cli.py
@@ -0,0 +1,24 @@
+from agent.agent import agent
+from langchain_core.runnables import RunnableConfig
+
+def run():
+    config: RunnableConfig = {"configurable": {"thread_id": "1"}}
+
+    print("Hi, I am Orion what you need today?")
+    print('Type "exit" to leave')
+
+    while True:
+        message = input("Your message: ")
+
+        if message.lower() == "exit":
+            break
+
+        if not message.strip():
+            continue
+
+        result = agent.invoke(
+            {"messages": [{"role":"user", "content":message}]},
+            config
+        )
+
+        print(f"Orion: {result['messages'][-1].content}")
