56b62d1a236723efe4c84fae3dfc33caa91b6f3d
Gabriel Darbello de Oliveira <gadarbello@gmail.com>
2026-05-30 19:38:29 -0300

    fix: set num_results on DuckDuckGoSearchResults

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,7 +1,8 @@
-from langchain_community.tools import DuckDuckGoSearchRun
+from langchain_community.tools import DuckDuckGoSearchResults
 from .calculator import calculator
 from .file_reader import read_file
+from config import MAX_SEARCH_RESULTS
 
-web_search = DuckDuckGoSearchRun()
+web_search = DuckDuckGoSearchResults(num_results=MAX_SEARCH_RESULTS)
 
 tools = [web_search, calculator, read_file]
